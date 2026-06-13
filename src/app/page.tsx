97b605cafe4dbdab45ab059468432a3473960d17
bhavdip-7span <bhavdip@7span.com>
2026-06-13 15:10:38 +0530

    [feat] add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import CTASection from "@/components/home/cta-section";
+import Footer from "@/components/home/footer";
+
+import HeroSection from "@/components/home/hero-section";
+import PopularBrandSection from "@/components/home/popular-brand-section";
 export default function Home() {
-  return <h1 className="text-primary bg-secondary">hello</h1>;
+  return (
+    <>
+      <HeroSection />
+      <PopularBrandSection />
+      <CTASection />
+      <Footer />
+    </>
+  );
 }
